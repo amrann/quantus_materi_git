b38890b2d3c1d15f57df468d46ad136258d5828f
Ndhel97 <58975431+Ndhel97@users.noreply.github.com>
2020-12-23 14:43:13 +0700

    update luas dan volume kubus, balok

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import luas
-import volume
-
+import luas_kubus_balok
+import volume_kubus_balok
 
 def test_luas_segitiga():
     result = luas.LuasSegititiga()
-    assert result.hitung(10,5)==23 , 'perhitungan salah'
+    assert result.hitung(10,5)==25 , 'perhitungan salah'
     assert result.hitung(10,-5)==25 , 'input salah'
 
+def test_luas_kubus():
+    result = luas_kubus_balok.LuasKubus()
+    assert result.hitung_LuasKubus(6)==216, 'perhitungan salah'
 
-def test_luas_prisma_segitiga():
-    luas_prismasegitiga = luas.PrismaSegitiga()
-    hasil = luas_prismasegitiga.hitung(6, 7, 5, 3, 4)
-    print ("Luas Prisma Segitiga = ", hasil)
+def test_luas_balok():
+    result = luas_kubus_balok.LuasBalok()
+    assert result.hitung_LuasBalok(3, 4, 5)==94, 'perhitungan salah'
 
-def test_volume_prisma_segitiga():
-    volume_prismasegitiga = volume.VolumePrismaSegitiga()
-    hasil = volume_prismasegitiga.hitung(6, 7)
-    print("Volume Prisma Segitiga = ", hasil)
-    
+def test_volume_kubus():
+    result = volume_kubus_balok.VolumeKubus()
+    assert result.hitung_VolumeKubus(5)==125, 'perhitungan salah'
 
-#test_luas_segitiga()
-test_luas_prisma_segitiga()
-test_volume_prisma_segitiga()
+def test_volume_balok():
+    result = volume_kubus_balok.VolumeBalok()
+    assert result.hitung_VolumeBalok(3, 4, 5)==60, 'perhitungan salah'
+
+
+
+test_luas_segitiga()
+test_luas_kubus()
+test_luas_balok()
+test_volume_kubus()
+test_volume_balok()
---
+++ volume_kubus_balok.py
@@ -0,0 +1,7 @@
+class VolumeBalok():
+    def hitung_VolumeBalok(self, panjang:float, lebar: float, tinggi:float):
+        return panjang*lebar*tinggi
+
+class VolumeKubus():
+    def hitung_VolumeKubus(self, sisi:float):
+        return sisi*sisi*sisi
