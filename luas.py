0724ef6fd79b71fec4f86dc345855ebd92ced98c
@meran <amr.bin.an@gmail.com>
2020-12-23 11:15:23 +0700

    first commit

--- luas.py
+++ luas.py
@@ -2,4 +2,4 @@ class LuasSegititiga():
     def hitung(self,alas:float,tinggi:float):
         return 0.5*alas*tinggi
 
-#kubus,balok,kerucut,bola,tabung,limas segitiga,prisma segitiga
+#kubus, balok, kerucut, bola, tabung, limas segitiga, prisma segitiga
