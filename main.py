f9944f2d5c6808685f7f7cc562d9b1bb217ccb0e
Reza Triawan <reza.triawan@gmail.com>
2020-12-23 19:04:36 +0700

    Perhitungan luas permukaan serta volume kerucut dan bola

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import luas
-import luas_kubus_balok
-import volume_kubus_balok
+#!/usr/bin/env python
+# coding: utf-8
 
-def test_luas_segitiga():
-    result = luas.LuasSegititiga()
-    assert result.hitung(10,5)==25 , 'perhitungan salah'
-    assert result.hitung(10,-5)==25 , 'input salah'
-
-def test_luas_kubus():
-    result = luas_kubus_balok.LuasKubus()
-    assert result.hitung_LuasKubus(6)==216, 'perhitungan salah'
-
-def test_luas_balok():
-    result = luas_kubus_balok.LuasBalok()
-    assert result.hitung_LuasBalok(3, 4, 5)==94, 'perhitungan salah'
-
-def test_volume_kubus():
-    result = volume_kubus_balok.VolumeKubus()
-    assert result.hitung_VolumeKubus(5)==125, 'perhitungan salah'
-
-def test_volume_balok():
-    result = volume_kubus_balok.VolumeBalok()
-    assert result.hitung_VolumeBalok(3, 4, 5)==60, 'perhitungan salah'
+# In[7]:
 
 
+import luas_kerucut_dan_bola
+import volume_kerucut_dan_bola
 
-test_luas_segitiga()
-test_luas_kubus()
-test_luas_balok()
-test_volume_kubus()
-test_volume_balok()
+
+# In[9]:
+
+
+def test_luas_kerucut():
+    result = luas_kerucut_dan_bola.LuasKerucut()
+    assert result.hitung(7,15)==483.56, 'perhitungan salah'
+    assert result.hitung(7,-15)==483.56, 'input salah'
+
+def test_luas_bola():
+    result = luas_kerucut_dan_bola.Luasbola()
+    assert result.hitung(5)==314, 'perhitungan salah'
+    assert result.hitung(-5)==314, 'input salah'
+
+def test_volume_kerucut():
+    result = volume_kerucut_dan_bola.VolumeKerucut()
+    assert result.hitung(3,10)==94.2, 'perhitungan salah'
+    assert result.hitung(3,-10)==94.2, 'input salah'
+
+def test_volume_bola():
+    result = volume_kerucut_dan_bola.VolumeBola()
+    assert result.hitung(3)==113.04, 'perhitungan salah'
+    assert result.hitung(-3)==113.04, 'input salah'
+
