05394d7dfc1c9d5d9afe7969b404d78825c3db79
Ndhel97 <hariz.dz97@gmail.com>
2020-12-23 18:18:47 +0700

    update test kubus balok

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import luas
 import volume
+import luas_kubus_balok
+import volume_kubus_balok
 
 
 def test_luas_segitiga():
@@ -18,7 +20,32 @@ def test_volume_prisma_segitiga():
     hasil = volume_prismasegitiga.hitung(6, 7)
     print("Volume Prisma Segitiga = ", hasil)
     
+def test_luas_kubus():
+    result = luas_kubus_balok.LuasKubus()
+    assert result.hitung_LuasKubus(6)==216, 'perhitungan salah'
+    print("Luas Kubus = ", result.hitung_LuasKubus(6))
+
+def test_luas_balok():
+    result = luas_kubus_balok.LuasBalok()
+    assert result.hitung_LuasBalok(3, 4, 5)==94, 'perhitungan salah'
+    print("Luas Balok = ", result.hitung_LuasBalok(3, 4, 5))
+
+def test_volume_kubus():
+    result = volume_kubus_balok.VolumeKubus()
+    assert result.hitung_VolumeKubus(5)==125, 'perhitungan salah'
+    print("Volume Kubus = ", result.hitung_VolumeKubus(5))
+
+def test_volume_balok():
+    result = volume_kubus_balok.VolumeBalok()
+    assert result.hitung_VolumeBalok(3, 4, 5)==60, 'perhitungan salah'
+    print("Volume Balok = ", result.hitung_VolumeBalok(3, 4, 5))
 
 #test_luas_segitiga()
 test_luas_prisma_segitiga()
 test_volume_prisma_segitiga()
+
+#test_kubus_balok
+test_luas_kubus()
+test_luas_balok()
+test_volume_kubus()
+test_volume_balok()
