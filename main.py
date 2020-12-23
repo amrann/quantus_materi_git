3bbbbcc3d62a8819b5f2c28c5119a5511d657fd8
@meran <amr.bin.an@gmail.com>
2020-12-23 14:14:03 +0700

    luas dan volume prisma segitiga

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import luas
+import volume
 
 
 def test_luas_segitiga():
     result = luas.LuasSegititiga()
-    assert result.hitung(10,5)==25 , 'perhitungan salah'
+    assert result.hitung(10,5)==23 , 'perhitungan salah'
     assert result.hitung(10,-5)==25 , 'input salah'
 
-test_luas_segitiga()
+
+def test_luas_prisma_segitiga():
+    luas_prismasegitiga = luas.PrismaSegitiga()
+    hasil = luas_prismasegitiga.hitung(6, 7, 5, 3, 4)
+    print ("Luas Prisma Segitiga = ", hasil)
+
+def test_volume_prisma_segitiga():
+    volume_prismasegitiga = volume.VolumePrismaSegitiga()
+    hasil = volume_prismasegitiga.hitung(6, 7)
+    print("Volume Prisma Segitiga = ", hasil)
+    
+
+#test_luas_segitiga()
+test_luas_prisma_segitiga()
+test_volume_prisma_segitiga()
