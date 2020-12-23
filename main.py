e6d2fe022abbc6e67de1958584263589ece2b400
fauzanmr <fauzanmrabbani@gmail.com>
2020-12-23 18:00:17 +0700

    Luas dan Volume Tabung, Limas Segitiga

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-import luas
+import luastabung_limassegitiga
+import volumetabung_limassegitiga
 
 
-def test_luas_segitiga():
-    result = luas.LuasSegititiga()
-    assert result.hitung(10,5)==25 , 'perhitungan salah'
-    assert result.hitung(10,-5)==25 , 'input salah'
+def test_luas_tabung():
+    result = luastabung_limassegitiga.LuasTabung()
+    assert result.hitung(10,5)==942
+#     assert result.hitung(10,-5)==25 , 'input luas tabung salah'
 
-test_luas_segitiga()
+
+
+def test_luas_limassegitiga():
+    result = luastabung_limassegitiga.LuasLimasSegitiga()
+    assert result.hitung(10,10,10,10,10,10,10,10)==200
+#     assert result.hitung(10,10,10,10,10,10,10,10)==25 , 'input luas limas segitiga salah'
+
+
+    
+def test_volume_tabung():
+    result = volumetabung_limassegitiga.VolumeTabung()
+    assert result.hitung(10,2)==628
+#     assert result.hitung(10,-5)==25 , 'input volume tabung salah'
+
+
+
+def test_volume_limassegitiga():
+    result = volumetabung_limassegitiga.VolumeLimasSegitiga()
+    assert result.hitung(3, 4, 100)==198
+#     assert result.hitung(3,4, 100)==25 , 'input volume limas segitiga salah'
+
+test_luas_tabung()
+test_luas_limassegitiga()
+test_volume_tabung()
+test_volume_limassegitiga()
